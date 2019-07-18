04cdc9c81560053ddaae742bc44f34d4fb6a441c
Marcus Almeida <malmeida@ardintel.com>
2019-07-18 14:56:42 -0400

    Updates

---
+++ precache-manifest.291a00f35609f7e200796574572e5c20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee8e178ab20bc690d2f7d4fab1749f1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "941e7ef5e538950bca0f",
+    "url": "/monsters-rolodex/static/css/main.55903a38.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "941e7ef5e538950bca0f",
+    "url": "/monsters-rolodex/static/js/main.b4495e73.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
